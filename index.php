84828c17cb5e47429a3ab6b330746e37013b70ba
Tainara Moraes <tainara@gmail.com>
2020-08-25 20:45:09 -0300

    Página index.php desenvolvida

---
+++ index.php
@@ -0,0 +1,18 @@
+<!Doctype html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Exercício-Fibonacci</title>
+    </head>
+    <body>
+        <form actio="calcula_sequencia_fibo.php" method="GET">
+            <fieldset>
+                <legend>Sequência de Fibonacci</legend>
+                    <label>Insire o número desejado:</label>
+                    <input type="number" name="valor_num"/>
+
+                    <input type="submit" name="submit" value="calcular"/>
+            </fieldset>
+        </form>
+    </body>
+</html>
